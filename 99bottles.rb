58632c36478579d26b9a4b98b7567e27efc7c7ca
Collette Grabill <ces84@drexel.edu>
2011-11-22 13:50:59 -0500

    use variables instead of repeating

--- 99bottles.rb
+++ 99bottles.rb
@@ -1,12 +1,15 @@
 int = 99
+text1 = ' bottles of beer on the wall'
+text2 = ' bottles of beer'
+text3 = ' take one down and pass it around'
 
 while true
-  #puts int -= + 1
-  puts int.to_s + ' bottles of beer on the wall'
-  puts int.to_s + ' bottles of beer'
-  puts 'if one  beer happens to fall'
+  #puts int -= + 1 #not needed just decrement
+  puts int.to_s + text1
+  puts int.to_s + text2 
+  puts text3
   puts int -= +1
-  puts ' bottles of beers on the wall'
+  puts text1
 
   request = gets.chomp
   
