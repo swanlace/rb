b26a18a799194d3bf959b796105026a39ea6547c
Collette Grabill <ces84@drexel.edu>
2011-11-22 10:09:47 -0500

    testing

---
+++ fave-num.rb
@@ -0,0 +1,7 @@
+puts 'What is your fave num?'
+num = gets.chomp
+puts num +   ' Is this your fave num? '
+answer = gets.chomp
+
+puts ' Great\'s' + ' that\'s a shitty number!'
+puts num.to_i + 10000 + ' This is a better number!'
