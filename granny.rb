b48d90c768aab1a404657df1bd8e588bcbd47366
Collette Grabill <ces84@drexel.edu>
2011-11-22 10:36:08 -0500

    cleaned up loop

--- granny.rb
+++ granny.rb
@@ -1,17 +1,15 @@
 granny= 'huh? speak up, sonny!'
 while true 
  input= gets.chomp
-puts granny.upcase
 
-if input!= input.upcase
- puts 'NO, NOT SINCE 1938!'
+if input != input.upcase
+  puts granny
 else 
- puts granny.upcase
+ puts 'NO, NOT SINCE 1938!'
 end 
 
 if input == 'bye'
  break
  end
-end 
-
+end
 
