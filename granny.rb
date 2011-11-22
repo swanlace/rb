31f34c518892efc5e427164775a970e885e8ba6b
swanlace <ces84@drexel.edu>
2011-11-22 10:33:04 -0500

    Update granny.rb

--- granny.rb
+++ granny.rb
@@ -1,2 +1,12 @@
-
-
+granny = 'huh? speak up sonny!'
+while true
+ input = gets.chomp
+if input !=input.upcase
+ puts granny
+else
+ puts 'NO, NOT SINCE 1938!'
+end 
+if input == 'bye'
+ break
+ end 
+end
